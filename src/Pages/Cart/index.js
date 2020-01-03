623bc9f3b5f05d263cceae78a785f298b27736ae
Arthur Oliveira <olvarthur@gmail.com>
2020-01-03 17:29:36 -0300

    Inicio Config de quantidade de pedido

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -97,7 +97,7 @@ function Cart({ products }) {
                       <button type="button">
                         <MdRemoveCircleOutline size={20} color="#7159c1" />
                       </button>
-                      <input type="number" readOnly value={product.amount} />
+                      <input type="number" value={product.amount} />
                       <button type="button">
                         <MdAddCircleOutline size={20} color="#7159c1" />
                       </button>
