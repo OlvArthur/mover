3a21b4369b51e576727afc152a326b686ec1d899
Arthur Oliveira <olvarthur@gmail.com>
2020-01-23 18:01:26 -0300

    First attempt for Login/Register deletion

--- src/Pages/LoginAndRegistration/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  font-family: sans-serif;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,10 @@ import { Switch, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
 import ProductForm from './Pages/ProductForm';
 import Cart from './Pages/Cart';
-import LoginAndRegister from './Pages/LoginAndRegistration';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={LoginAndRegister} />
       <Route path="/landingpage" component={LandingPage} />
       <Route path="/form" component={ProductForm} />
       <Route path="/cart" component={Cart} />
