9776e5bbb41b75e1fd6535a1161537f573a61219
Arthur Oliveira <olvarthur@gmail.com>
2020-01-27 22:59:37 -0300

    Register part addition

--- src/Pages/Session/index.js
+++ src/Pages/Session/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
-import { Container, Header, FlexGrid } from './styles';
+import {
+  Container,
+  Header,
+  FlexGrid,
+  ForgotLink,
+  Form,
+  Half,
+  Divider,
+  RegisterLink,
+} from './styles';
 
 export default function Session() {
   return (
@@ -11,18 +20,52 @@ export default function Session() {
       </Header>
 
       <FlexGrid>
-        <div>
-          <form>
-            <div>
-              <label>Email</label>
-              <input type="text" name="email" required="required" />
-            </div>
-            <div>
-              <label>Password</label>
-              <input name="password" type="password" required="required" />
-            </div>
-          </form>
-        </div>
+        <Half>
+          <div>
+            <Form>
+              <div>
+                <label>Email</label>
+                <input type="text" name="email" required="required" />
+              </div>
+              <div>
+                <label>Password</label>
+                <input name="password" type="password" required="required" />
+              </div>
+
+              <div>
+                <button type="button">
+                  <span>Log In</span>
+                </button>
+
+                <ForgotLink to="/forgotPassword">Forgot Password?</ForgotLink>
+              </div>
+            </Form>
+          </div>
+        </Half>
+        <Divider>
+          <div name="top-bar" />
+          <span>OR</span>
+          <div name="bottom-bar" />
+        </Divider>
+
+        <Half>
+          <div>
+            <label>Email</label>
+            <input type="text" name="email" />
+          </div>
+
+          <div>
+            <label>Nome da Empresa</label>
+            <input type="text" />
+          </div>
+
+          <div>
+            <label>Senha</label>
+            <input type="password" name="password" />
+          </div>
+        </Half>
+
+        <RegisterLink to="/Register" />
       </FlexGrid>
     </Container>
   );
--- src/Pages/Session/styles.js
+++ src/Pages/Session/styles.js
@@ -1,35 +1,61 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
-  position: relative;
-  z-index: auto;
-  max-width: 800px;
-  margin: 0 auto;
-  width: 100%;
   padding: 20px;
+  padding-left: 25%;
+  padding-right: 25%;
+
+  color: white;
 `;
 
 export const Header = styled.header`
+  margin: 50px 0 20px 0;
   box-sizing: border-box;
   display: block;
   float: none;
-  margin: 50px 0 20px 0;
+  color: white;
 
   h1 {
     margin: 0 0 0 0 -6px;
     font: 0/0 a;
   }
+
+  h2 {
+    font-size: 4.5rem;
+    font-weight: normal;
+  }
 `;
 
 export const FlexGrid = styled.div`
-  box-sizing: border-box;
-  display: flex;
-  float: none;
+  display: grid;
+  grid-template-columns: auto auto auto;
+  flex-wrap: wrap;
   line-height: 22.5px;
-  position: static;
-  z-index: auto;
   margin: 50px 0px 20px;
   border: none;
+
+  div {
+    padding-right: 10%;
+    box-sizing: border-box;
+  }
+`;
+
+export const ForgotLink = styled(Link)`
+  text-decoration: none;
+  text-align: center;
+  display: block;
+`;
+
+export const Half = styled.div``;
+
+export const Divider = styled.div``;
+
+export const RegisterLink = styled(Link)``;
+
+export const Form = styled.form`
+  width: 55%;
+  display: block;
 
   div {
   }
