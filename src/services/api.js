c490e8fa2ca180050d9fcd951d9d0a5a91046268
Arthur Oliveira <olvarthur@gmail.com>
2020-02-18 17:51:32 -0300

    Change to api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://www.api.intregra.com:3333',
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://www.api.intregra.com:3333',
+  // baseURL: 'http://localhost:3333',
 });
 
 export default api;
