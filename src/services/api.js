6296c8404b085fb8ebe23e21d129fc7fe7d184a0
Arthur Oliveira <olvarthur@gmail.com>
2020-02-20 11:15:07 -0300

    Changing api to https

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://www.api.intregra.com:3333',
+  baseURL: 'https://api.intregra.com',
   // baseURL: 'http://localhost:3333',
 });
 
